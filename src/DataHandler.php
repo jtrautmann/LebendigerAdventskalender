f84eef2957c68d2f164a9c7f107438d9bb54c31e
Jeremias Trautmann <jeremias@jtraut.com>
2019-09-24 17:36:23 +0200

    Added database deletion on plugin deactivation.

--- src/DataHandler.php
+++ src/DataHandler.php
@@ -2,18 +2,28 @@
 
 class DataHandler {
 
-    public function initializeDatabase() {
+    private $project_name = "lebendigeradventskalender";
+    private $hosts_table_name;
+    private $participants_table_name;
+
+    public function __construct($project_name) {
         global $wpdb;
 
-        // set names of the new tables
-        $hosts_table_name = $wpdb->prefix . "lebendigeradventskalender_hosts";
-        $participants_table_name = $wpdb->prefix . "lebendigeradventskalender_participants";
+        $db_prefix = $wpdb->prefix . $project_name . "_";
+        
+        // set names of the tables
+        $hosts_table_name = $db_prefix . "hosts";
+        $participants_table_name = $db_prefix . "participants";
+    }
+
+    public function initializeDatabase() {
+        global $wpdb;
 
         // ---- create tables ----
         $charset_collate = $wpdb->get_charset_collate();
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');    // needed for dbDelta function
         
-        // create hosts tables
+        // create hosts table
         $sql = "CREATE TABLE $hosts_table_name (
                     day tinyint(1) unsigned NOT NULL,
                     year smallint(4) unsigned NOT NULL, 
@@ -32,7 +42,7 @@ class DataHandler {
           
           dbDelta($sql);
 
-        // create participants tables
+        // create participants table
         $sql = "CREATE TABLE $participants_table_name (
                     day tinyint(1) unsigned NOT NULL,
                     year smallint(4) unsigned NOT NULL,
@@ -42,10 +52,23 @@ class DataHandler {
                 ) $charset_collate;";
         
         dbDelta($sql);
+
+        // add version number for updates
+        add_option($this->project_name . "_db_version", "1.0" );
     }
 
     public function deleteDatabase() {
-        // TODO
+        global $wpdb;
+
+        // delete tables
+        $sql = "DROP TABLE IF EXISTS $this->hosts_table_name;";
+        $wpdb->query($sql);
+
+        $sql = "DROP TABLE IF EXISTS $this->participants_table_name;";
+        $wpdb->query($sql);
+
+        // delete version number
+        delete_option($this->project_name . "_db_version");
     }
 
 }
